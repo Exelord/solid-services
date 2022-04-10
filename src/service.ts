14451c1b94afeadb6961b36916c552046df7d73b
Maciej Kwaśniak <contact@exelord.com>
2022-04-10 17:15:58 +0200

    Improve service typing

--- src/service.ts
+++ src/service.ts
@@ -1,9 +1,11 @@
 import { useRegistry } from "./context";
 import type { ServiceInitializer, Service } from "./registry";
 
+export type ServiceGetter<T extends Service> = () => T;
+
 export function useService<T extends Service>(
   initializer: ServiceInitializer<T>
-): () => T {
+): ServiceGetter<T> {
   const registry = useRegistry();
   return () => registry.get(initializer) || registry.register(initializer);
 }

34fa362aa1bdad8f7a924903ce2c320bcc3cf48a
Maciej Kwaśniak <contact@exelord.com>
2022-12-29 20:30:39 +0100

    Update project config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import path from "path";
 import { defineConfig } from "vite";
 
-module.exports = defineConfig({
+export default defineConfig({
   build: {
     target: "esnext",
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
       formats: ["cjs", "es"],
-      fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
       external: ["solid-js"],

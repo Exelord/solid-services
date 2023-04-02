fabc2ad3e8b8670263391c15509f3ab4b52cacff
Maciej Kwaśniak <contact@exelord.com>
2023-04-02 16:49:20 +0200

    Fix build config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   build: {
     target: "esnext",
+    minify: false,
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
       formats: ["cjs", "es"],
